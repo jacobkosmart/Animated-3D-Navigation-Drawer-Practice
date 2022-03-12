016903bf0df17df016d6e94395e3b0a951e449c5
jacobkosmart <jacobkosmart@gmail.com>
2022-03-12 19:12:44 +0900

    Initial Commit

---
+++ navigation3DDrawer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  navigation3DDrawer
+//
+//  Created by Jacob Ko on 2022/03/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ navigation3DDrawer/navigation3DDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigation3DDrawerApp.swift
+//  navigation3DDrawer
+//
+//  Created by Jacob Ko on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct navigation3DDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
